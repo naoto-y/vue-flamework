998779cccb61ac4320effc9e2a3962a3c2ad9c65
n.yoshida <inazuma074@gmail.com>
2018-12-23 22:10:38 +0900

    update app.js & add top

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
-var app = new Vue({
+Vue.component('page', {
+    template: '<div class="page"><vueMain /></div',
+    data:function(){
+        return {
+            routing: null,
+        }
+    },
+    created:function(){
+        console.log('page');
+    },
+    methods:{
+        loadRouting:function(){
+            this.routing = 'hoge';
+        }
+    }
+})
+
+Vue.component('vueMain', {
+    template: '<div>main</div>',
+    created:function(){
+        console.log('main');
+    }
+})
+
+Vue.component('myh1', {
+    template: '<h1 :id="id" class="myh1">{{data}}</h1>',
+    props: {id:null,data:null},
+    methods: {
+        color: function(text){
+            echo('color: ${text};');
+        }
+    }
+});
+
+const app = new Vue({
     el: '#app',
     data: {
     },
+    beforeCreate:function(){
+    },
+    mounted:function(){
+    },
+    methods:{
+
+    }
 })
